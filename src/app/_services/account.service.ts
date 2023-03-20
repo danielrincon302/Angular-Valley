ffa9b1f0d7b16853700437f91d190dc104d67c38
danielrincon302 <danielrincon@live.com>
2023-03-20 18:31:27 -0500

    Update account.service.ts

--- src/app/_services/account.service.ts
+++ src/app/_services/account.service.ts
@@ -1,4 +1,4 @@
-﻿import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
@@ -43,6 +43,91 @@ export class AccountService {
                 //return x;
             }));
     }
+
+    getNotify(id: string) {
+        return this.http.get<any>(`http://127.0.0.1:8000/api/apiNotifyShow/${id}`);
+    }
+
+    getNotifyUnique(id: BigInteger) {
+        return this.http.get<any>(`http://127.0.0.1:8000/api/apiNotifyUnique/${id}`);
+    }
+
+    updateNotify(id: BigInteger, params: any) {
+        //params, directo nota
+        console.log("Params: " + params["nota"])
+        const body = { nota: ' ' };
+        body["nota"] = params["nota"];
+        this.http.put<any>(`http://127.0.0.1:8000/api/apiNotifyUpdate/${id}`, body).subscribe(data => {
+            })
+    }
+
+    postNotify(params: any) {
+        //params, directo nota
+        console.log("Params call: " + params["nota"])
+        console.log("Params id: " + params["id"])
+        const body = { nota: ' ',  id: 0};
+        //se envia el id fk
+        body["nota"] = params["nota"];
+        body["id"] = params["id"];
+        return this.http.post<any>(`http://127.0.0.1:8000/api/apiNotifyNew/`, body);
+    }
+
+    login(username: string, password: string) {
+        return this.http.post<User>(`${environment.apiUrl}/users/authenticate`, { username, password })
+            .pipe(map(user => {
+                // store user details and jwt token in local storage to keep user logged in between page refreshes
+                localStorage.setItem('user', JSON.stringify(user));
+                this.userSubject.next(user);
+                return user;
+            }));
+    }
+
+    logout() {
+        // remove user from local storage and set current user to null
+        localStorage.removeItem('user');
+        this.userSubject.next(null);
+        this.router.navigate(['/account/login']);
+    }
+
+    register(user: User) {
+        return this.http.post(`${environment.apiUrl}/users/register`, user);
+    }
+
+    getAll() {
+        return this.http.get<User[]>(`${environment.apiUrl}/users`);
+    }
+
+    getById(id: string) {
+        return this.http.get<User>(`${environment.apiUrl}/users/${id}`);
+    }
+
+    update(id: string, params: any) {
+        return this.http.put(`${environment.apiUrl}/users/${id}`, params)
+            .pipe(map(x => {
+                // update stored user if the logged in user updated their own record
+                if (id == this.userValue?.id) {
+                    // update local storage
+                    const user = { ...this.userValue, ...params };
+                    localStorage.setItem('user', JSON.stringify(user));
+
+                    // publish updated user to subscribers
+                    this.userSubject.next(user);
+                }
+                return x;
+            }));
+    }
+
+    delete(id: string) {
+        return this.http.delete(`${environment.apiUrl}/users/${id}`)
+            .pipe(map(x => {
+                // auto logout if the logged in user deleted their own record
+                if (id == this.userValue?.id) {
+                    this.logout();
+                }
+                return x;
+            }));
+    }
+}
 
     getNotify(id: string) {
         return this.http.get<any>(`http://127.0.0.1:8000/api/apiNotifyShow/${id}`);
